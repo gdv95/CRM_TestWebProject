91d23064ca0a570c639b22f17a1573ae3500caea
gdv <gdv@NROWS087>
2021-06-18 10:18:55 +0700

    added delete buttons

--- CRM_TestWebProject/Controllers/HomeController.cs
+++ CRM_TestWebProject/Controllers/HomeController.cs
@@ -8,6 +8,29 @@ namespace CRM_TestWebProject.Controllers
 {
     public class HomeController : Controller
     {
+        public List<CRMUser> Users
+        {
+            get
+            {
+                if (Session["tmp"] == null)
+                {
+                    /*Session["tmp"] = new List<CRMUser>
+                    {
+                        new CRMUser("FirstName1", "Lastname1", "Login1"),
+                        new CRMUser("FirstName2", "Lastname2", "Login2"),
+                        new CRMUser("FirstName3", "Lastname3", "Login3")
+                    };*/
+                    Session["tmp"] = UnionProvider.getUsers();
+                }
+                return Session["tmp"] as List<CRMUser>;
+            }
+            set
+            {
+                Session["tmp"] = value;
+            }
+
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -29,8 +52,25 @@ namespace CRM_TestWebProject.Controllers
 
         public ActionResult List()
         {
-            var list = UnionProvider.getUsers();
-            return View(list);
+            // temp
+            return View(Users);
+        }
+
+        public ActionResult Delete(string i)
+        {
+            var st = Users.Find(c => c.Login == i);
+            Users.Remove(st);
+            return View("List", Users);
+
+            /*var st = lst.Find(c => c.Login == i);
+            lst.Remove(st);
+            return View("Index", lst);*/
+        }
+
+        public ActionResult DeleteAll()
+        {
+            Users.Clear();
+            return View("List", Users);
         }
     }
 }
---
+++ CRM_TestWebProject/Controllers/TestController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRM_TestWebProject.Controllers
+{
+    public class TestController : Controller
+    {
+        //List<CRMUser> lst;
+
+        public TestController()
+        {
+            /*lst = new List<CRMUser>
+            {
+            new CRMUser("FirstName1", "Lastname1", "Login1"),
+            new CRMUser("FirstName2", "Lastname2", "Login2"),
+            new CRMUser("FirstName3", "Lastname3", "Login3")
+            };*/
+        }
+
+        public List<CRMUser> Students
+        {
+            get
+            {
+                if (Session["tmp"] == null)
+                {
+                    Session["tmp"] = new List<CRMUser>
+           {
+            new CRMUser("FirstName1", "Lastname1", "Login1"),
+            new CRMUser("FirstName2", "Lastname2", "Login2"),
+            new CRMUser("FirstName3", "Lastname3", "Login3")
+           };
+                }
+                return Session["tmp"] as List<CRMUser>;
+            }
+            set
+            {
+                Session["tmp"] = value;
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View(Students);
+        }
+
+        public ActionResult Delete(string i)
+        {
+            var st = Students.Find(c => c.Login == i);
+            Students.Remove(st);
+            return View("Index", Students);
+
+            /*var st = lst.Find(c => c.Login == i);
+            lst.Remove(st);
+            return View("Index", lst);*/
+        }
+    }
+}
